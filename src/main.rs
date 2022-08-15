b4f16e225a1697415bf1da73e98142c81b15ffa2
nireo <eemil.lehtikangas1@gmail.com>
2022-08-15 15:24:49 +0300

    fixed a small bug with the server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::sync::Arc;
-
 use anyhow::Result;
-use rpc::proto::rpc_server::RpcServer;
+use std::sync::Arc;
 use tonic::transport::Server;
 
 pub mod error;
@@ -16,32 +14,34 @@ pub use server::StoreTransport;
 
 use structopt::StructOpt;
 use rpc::{RpcService, RpcTransport};
+pub use rpc::proto::rpc_server::RpcServer;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "server")]
+#[structopt(name = "gouged")]
 struct Opt {
+    /// The ID of this server.
     #[structopt(short, long)]
     id: usize,
-
+    /// The IDs of peers.
     #[structopt(short, long, required = false)]
     peers: Vec<usize>,
 }
 
-fn get_node_data(id: usize) -> (&'static str, u16) {
+fn node_data(id: usize) -> (&'static str, u16) {
     let host = "127.0.0.1";
     let port = 50000 + (id as u16);
     (host, port)
 }
 
-fn get_node_addr(id: usize) -> String {
-    let (host, port) = get_node_data(id);
+fn node_rpc_addr(id: usize) -> String {
+    let (host, port) = node_data(id);
     format!("http://{}:{}", host, port)
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let opt = Opt::from_args();
-    let (host, port) = get_node_data(opt.id);
+    let (host, port) = node_data(opt.id);
     let rpc_listen_addr = format!("{}:{}", host, port).parse().unwrap();
     let transport = RpcTransport::new(Box::new(node_rpc_addr));
     let server = StoreServer::start(opt.id, opt.peers, transport)?;
